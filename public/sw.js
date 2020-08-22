1d02fe059b20e26da318baf6db684af0eefd2544
Abdurrahman Asad <bsef17m005@pucit.edu.pk>
2020-08-22 11:16:25 +0500

    //cache fixes

--- public/sw.js
+++ public/sw.js
@@ -7,8 +7,8 @@ var urlsToCache = [
   '/static/js/main.4894409e.chunk.js',
   '/static/js/2.0bf25218.chunk.js',
   '/static/css/main.b1b794a2.chunk.css',
+  '/sw.js',
   '/index.html',
-  '/sockjs-node',
   '/et.png',
   '/et1.png',
   '/manifest.json',
@@ -24,41 +24,41 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request)
-        .catch(()=>caches.match('/offline.html'));
-      }
-    )
-  );
-});
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         if (response) {
+//           return response;
+//         }
+//         return fetch(event.request)
+//         .catch(()=>caches.match('/offline.html'));
+//       }
+//     )
+//   );
+// });
 
-  // self.addEventListener('fetch', function(event) {
-  //   event.respondWith(
-  //     caches.match(event.request)
-  //       .then(function(response) {
-  //         if (response) {
-  //           return response;
-  //         }
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;
+          }
   
-  //         return fetch(event.request).then(
-  //           function(response) {
-  //             if(!response || response.status !== 200 || response.type !== 'basic') {
-  //               return response;
-  //             }
-  //             var responseToCache = response.clone();
-  //             caches.open(CACHE_NAME)
-  //               .then(function(cache) {
-  //                 cache.put(event.request, responseToCache);
-  //               });
-  //             return response;
-  //           }
-  //         );
-  //       })
-  //     );
-  // });
+          return fetch(event.request).then(
+            function(response) {
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+              var responseToCache = response.clone();
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+              return response;
+            }
+          );
+        })
+      );
+  });
